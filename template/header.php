b4870bd58805412d49076e309a7a3d6464b3d6c6
Bruno Alves Lourenço <bruunieng@gmail.com>
2020-12-08 23:56:52 -0300

    [PR6]Ajuste em como encontrar e inserção de pasta template

---
+++ template/header.php
@@ -0,0 +1,28 @@
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>Bruno Alves Lourenço - Site</title>
+    <meta name="theme-color" content="rgb(78,29,29)">
+    <meta name="twitter:card" content="">
+    <meta name="twitter:image" content="./assets/img/Profile.jpg?h=cb206b0efec9f7e07a7b28e95247cd24">
+    <meta property="og:image" content="./assets/img/Profile.jpg?h=cb206b0efec9f7e07a7b28e95247cd24">
+    <meta name="description" content="Desenvolvedor de Sistema">
+    <meta property="og:type" content="website">
+    <link rel="icon" type="image/png" sizes="140x52" href="./assets/img/BrunoLogof.png?h=359f9d9c3d569f64e71890bbeeb99543">
+    <link rel="icon" type="image/png" sizes="140x52" href="./assets/img/BrunoLogof.png?h=359f9d9c3d569f64e71890bbeeb99543">
+    <link rel="icon" type="image/png" sizes="140x52" href="./assets/img/BrunoLogof.png?h=359f9d9c3d569f64e71890bbeeb99543">
+    <link rel="icon" type="image/png" sizes="140x52" href="./assets/img/BrunoLogof.png?h=359f9d9c3d569f64e71890bbeeb99543">
+    <link rel="icon" type="image/png" sizes="140x52" href="./assets/img/BrunoLogof.png?h=359f9d9c3d569f64e71890bbeeb99543">
+    <link rel="stylesheet" href="./assets/bootstrap/css/bootstrap.min.css?h=5b32bc9c536a9edeab10f4bcdadceedf">
+    <link rel="manifest" href="/manifest.json?h=af8f84990ed10c49331279fe006b1060">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat:400,400i,700,700i,600,600i">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lora">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
+    <link rel="stylesheet" href="./assets/fonts/fontawesome5-overrides.min.css?h=cfa788754642173dafbd830bd7969fdb">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.10.0/baguetteBox.min.css">
+    <link rel="stylesheet" href="./assets/css/styles.min.css?h=ffaabd17fdd578c962dbce4728c0c294">
+    <link rel="stylesheet" href="./assets/css/index.css">
+</head>
+<!-- content -->
