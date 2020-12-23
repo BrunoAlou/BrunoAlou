c27791a08df83be174cd87889f59aa5240d888ab
Bruno Alves Lourenço <bruunieng@gmail.com>
2020-12-23 09:38:12 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,43 @@
+    <!-- Start: Article Dual Column -->
+    <div class="article-dual-column">
+        <div class="container">
+            <div class="row">
+                <div class="col"></div>
+                <div class="col-md-10 offset-md-1">
+                    <!-- Start: Intro -->
+                    <div class="intro">
+                        <h1 class="text-center">Sobre Mim</h1>
+                        <!-- Start: Date and Author -->
+                        <p class="text-center"><span class="by">by</span> <a href="#">BRUNO aLVES lOURENÇO</a><span class="date">Sept 28th, 2019</span></p>
+                        <!-- End: Date and Author -->
+                    </div>
+                    <!-- End: Intro -->
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-10 col-lg-3 offset-md-1">
+                    <!-- Start: Toc -->
+                    <div class="toc">
+                        <ul></ul>
+                    </div>
+                    <!-- End: Toc --><img src="./assets/img/Profile.jpg?h=cb206b0efec9f7e07a7b28e95247cd24" width="150"></div>
+                <div class="col-md-10 col-lg-7 offset-md-1 offset-lg-0">
+                    <!-- Start: Text -->
+                    <div class="text">
+                        <p>Natural de Goiânia, Goiás.</p>
+                        <p>Fanático por tecnologia e inquito por natureza, curso Engenharia de Computação no Centro Universitário Uni-Anhanguera e atuo como Desenvolvedor de sistemas na startup&nbsp;<a href="https://www.appfacilita.com/">Facilita</a>.</p>
+                        <p>Adoro jogos e esportes, já atuei como coach de League of Legends fundei a equipe de jogos da instituição(Heaven To Seven).</p>
+                        <p>Atualmente engajado em criação de conteúdo para desenvolvedores e simpatizantes da área de tecnologia, minhas postagens podem ser encontradas no Medium ou nos Artigos do Linkedin.</p>
+                        <p>Atual Co-Host do FacilitaTech, podcast de tecnologia da empresa Facilita.</p>
+                    </div>
+                    <!-- End: Text -->
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- End: Article Dual Column -->
+    <?php include('footer.php'); ?>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.10.0/baguetteBox.min.js"></script>
+    <script src="./assets/js/script.min.js?h=a600b8e7e9619265383470da5f98b4f6"></script>
